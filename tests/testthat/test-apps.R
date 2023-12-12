dcbca0ad5e39a17a81f1c72160010252a12e8e3f
BF <bobby@happycabbage.io>
2023-12-12 11:25:21 -0800

    added expectation to avoid skip test warning

--- tests/testthat/test-apps.R
+++ tests/testthat/test-apps.R
@@ -2,11 +2,11 @@
 library(shinytest2)
 
 test_that("Testing all package apps", {
-  skip_on_cran() # Don't run these tests on the CRAN build servers
-  skip_on_ci() # Skip this on CI since we have a dedicated workflow for testing apps
-  fs::path_package("rblogapps", "apps") |>
-    fs::dir_ls(type = "directory") |>
-    lapply(test_app)
+  skip_if(is_ci(), "On CI") # Skip CI bc we have a dedicated workflow for apps
+  paths <- fs::path_package("rblogapps", "apps") |>
+    fs::dir_ls(type = "directory")
+  lapply(paths, test_app) |>
+    expect_named(as.character(paths))
 })
 
 
